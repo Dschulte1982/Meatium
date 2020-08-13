c7fff1d625e0e0e580a9f42fc8b3ed11b6f8d9d0
Michelle <mz0809445@gmail.com>
2020-08-13 04:03:22 -0400

    edited the header of welcome page

--- routes/pages.js
+++ routes/pages.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 const csrfProtection = require("csurf")({ cookie: true });
 
-
 router.get('/login', csrfProtection, (req, res) => {
   if (req.user) {
     res.redirect('/');
     return;
   }
   res.render('login', { csrf: req.csrfToken() });
+});
+
+router.get('/', csrfProtection, (req, res) => {
+  if (req.user) {
+    res.redirect('/');
+    return;
+  }
+  res.render('welcome', { csrf: req.csrfToken() });
 });
 
 router.get('/signup', csrfProtection, (req, res) => {
