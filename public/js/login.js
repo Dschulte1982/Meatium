69ad510f388ef5cc4f12e82a6cef5d770f66e7c8
Dustin Schulte <dschulte1982@hotmail.com>
2020-08-14 11:14:12 -0700

    Added functionality to the demo user login

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('#login-form');
+const submitButton = document.querySelector('#submit-button');
+const demoButton = document.querySelector('#demo-button')
 
-
-form.addEventListener('submit', async (e) => {
+//Event for clicking the form button
+submitButton.addEventListener('click', async (e) => {
   console.log('submitting');
   e.preventDefault();
   const formData = new FormData(form);
@@ -26,6 +28,32 @@ form.addEventListener('submit', async (e) => {
     errorsContainer.innerHTML = message;
     return;
   }
+  window.location.href = '/home';
+});
 
+//Event for logging in directly to the Demo User account
+demoButton.addEventListener('click', async (e) => {
+  console.log('logging in as Demo User');
+  e.preventDefault();
+
+  const username = 'DemoUser';
+  const password = 'password';
+  const body = { username, password, };
+
+  const res = await fetch('/api/users/demo', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    const { message } = data;
+    const errorsContainer = document.querySelector('#errors-container');
+    errorsContainer.innerHTML = message;
+    return;
+  }
   window.location.href = '/home';
 });
